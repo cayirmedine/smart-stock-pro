3e43794965f80d54231fe0b1584289d85596b918
cayirmedine <htc.mdn.2010@gmail.com>
2026-03-01 16:26:58 +0300

    feat: initialize express server with cors and health endpoint

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from './config/env';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: env.CORS_ORIGIN,
+  credentials: env.CORS_CREDENTIALS,
+}));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(env.APP_PORT, env.APP_HOST, () => {
+  console.log(`${env.APP_NAME} is running on http://${env.APP_HOST}:${env.APP_PORT} in ${env.NODE_ENV} mode`);
+});
+
+export default app;
