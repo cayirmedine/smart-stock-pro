980a817966d4153a3296dc7378afb6660bb62e1d
cayirmedine <htc.mdn.2010@gmail.com>
2026-03-12 00:30:20 +0300

    feat: Initialize Sentry SDK configuration

---
+++ src/lib/sentry.ts
@@ -0,0 +1,10 @@
+import * as Sentry from "@sentry/node";
+import { env } from '../config/env';
+
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true,
+});
+
+export default Sentry;
